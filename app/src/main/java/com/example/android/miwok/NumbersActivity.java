fe7e2c1daeca563fa607e49fe29ee79a7968be26
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-04-27 11:25:22 -0300

    - update the code to use audio focus

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,7 +35,39 @@ import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    private Context mContext = getApplicationContext();
     private MediaPlayer mediaPlayer;
+    private AudioManager am;
+    private AudioManager.OnAudioFocusChangeListener afChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Pause playback because your Audio Focus was
+                        // temporarily stolen, but will be back soon.
+                        // i.e. for a phone call
+                        mediaPlayer.pause();
+                        mediaPlayer.seekTo(0);
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        // Stop playback, because you lost the Audio Focus.
+                        // i.e. the user started some other playback app
+                        // Remember to unregister your controls/buttons here.
+                        // And release the kra — Audio Focus!
+                        // You’re done.
+                        releaseMediaPlayer();
+                    }  else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Resume playback, because you hold the Audio Focus
+                        // again!
+                        // i.e. the phone call ended or the nav directions
+                        // are finished
+                        // If you implement ducking and lower the volume, be
+                        // sure to return it to normal here, as well.
+                        mediaPlayer.start();
+                    }
+                }
+            };
+
 
     /**
      * This listener gets triggered when the {@link MediaPlayer} has completed
@@ -51,6 +84,9 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // create and setup the {@link AudioManager} to request audio focus
+        am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
 
         // create an ArrayList of the Word type
         final ArrayList<Word> words = new ArrayList<>();
@@ -87,16 +123,25 @@ public class NumbersActivity extends AppCompatActivity {
                     // release the media player if it currently exists
                     releaseMediaPlayer();
 
-                    // create and setup the {@link MediaPlayer} for the audio resource
-                    // associate with the current word
-                    mediaPlayer = MediaPlayer.create(getApplicationContext(), words.get(position).getmSoundResourceId());
+                    // request audio focus for playback
+                    int result = am.requestAudioFocus(
+                            afChangeListener,
+                            AudioManager.STREAM_MUSIC,
+                            AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
 
-                    // start play the sound
-                    mediaPlayer.start();
+                    if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                        // create and setup the {@link MediaPlayer} for the audio resource
+                        // associate with the current word
+                        mediaPlayer = MediaPlayer.create(getApplicationContext(), words.get(position).getmSoundResourceId());
 
-                    // Setup a listener on the media player, so that we can stop and
-                    // release the media player once the sounds has finished.
-                    mediaPlayer.setOnCompletionListener(mCompletionListener);
+                        // start play the sound
+                        mediaPlayer.start();
+
+                        // Setup a listener on the media player, so that we can stop and
+                        // release the media player once the sounds has finished.
+                        mediaPlayer.setOnCompletionListener(mCompletionListener);
+                    }
+
 
                 } else {
                     Toast.makeText(getApplicationContext(), "No sound available", Toast.LENGTH_SHORT).show();
@@ -114,6 +159,9 @@ public class NumbersActivity extends AppCompatActivity {
         if (mediaPlayer != null) {
             mediaPlayer.release();
             mediaPlayer = null;
+
+            // After finish the sound, abandon the audio focus
+            am.abandonAudioFocus(afChangeListener);
         }
     }
 
